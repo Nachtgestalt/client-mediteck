cd5dab0f1de2da269ca8990219e6bdffaf5a0946
Sebastian Vargas Barrera <sebastian.vargas@interglobalcs.com>
2018-07-24 12:00:54 -0500

    Google Evetns

--- src/app/pages/medical-consultations/dates/dates.component.ts
+++ src/app/pages/medical-consultations/dates/dates.component.ts
@@ -17,6 +17,14 @@ export class DatesComponent implements OnInit {
   
 
   constructor(public http: HttpClient){
+    //Google Auth
+    if(window.location.href.indexOf("access_token") > -1){
+      let url = window.location.href;  
+      let au = url.split('access_token=')[1].split('&token_type')[0];
+      localStorage.setItem('google_token', au);
+      window.location.href = this.redirect_uri;
+    }
+
     let google = localStorage.getItem('google_token');
     if(google){
       this.visible = false;
@@ -24,15 +32,7 @@ export class DatesComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
-    //Google Auth
-    if(window.location.href.indexOf("access_token") > -1){
-      let url = window.location.href;  
-      let au = url.split('access_token=')[1].split('&token_type')[0];
-      localStorage.setItem('google_token', au);
-      this.getDates();
-    }
-  }
+  ngOnInit(){}
 
   public googleAuth() {
     let url = `https://accounts.google.com/o/oauth2/auth?client_id=${this.client_id}&redirect_uri=${this.redirect_uri}&scope=https://www.googleapis.com/auth/calendar&approval_prompt=force&response_type=token`;
@@ -40,7 +40,7 @@ export class DatesComponent implements OnInit {
   }
 
   public getDates(){
-    if(localStorage.getItem('google_calendar_id')){
+    if(!localStorage.getItem('google_calendar_id')){
       let url = "https://www.googleapis.com/calendar/v3/users/me/calendarList"
       this.gDates = this.http.get(url,{
         headers: {
