19b315e44e1a9b43c8621632930b28e699cf4720
Sebastian Vargas Barrera <sebastian.vargas@interglobalcs.com>
2018-08-11 00:49:39 -0500

    MailChimp

--- src/app/services/doctor/doctor.service.ts
+++ src/app/services/doctor/doctor.service.ts
@@ -31,6 +31,23 @@ export class DoctorService {
     headers = headers.append('Authorization', this.token);
     headers = headers.append('Content-Type', 'application/json');
     const body = JSON.stringify(doctor);
+
+    console.log(doctor);
+    //MailChimp
+    let apiKey = "53124a9ef44b68509728c7d3fd9e32be-us19";
+    let listId = "fe8a39b54b";
+    let dc = apiKey.split("-")[1];
+    let ere = {
+      email_address: doctor.Email,
+      status: "subscribed"
+    }
+    this.http.post(`https://${dc}.api.mailchimp.com/3.0/lists/${listId}/members`, ere, {
+      headers: {
+        "Authorization":`Basic ${btoa(`anystring:${apiKey}`)}`
+      }
+    });
+    //MailChimp
+
     return this.http.post(this.urlDoctor, body, {headers});
   }
 
