d9804e6d242f1cd14c9b028bd89c1f6ab6b04186
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2020-06-29 18:03:54 -0500

    Fix mensajes de notificacion en urgencias

--- src/app/modals/send-to-urgencies/send-to-urgencies.component.ts
+++ src/app/modals/send-to-urgencies/send-to-urgencies.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Input} from '@angular/core';
 import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import {HttpClient} from '@angular/common/http';
 import {URL_SERVICIOS} from '../../config/config';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-send-to-urgencies',
@@ -10,38 +11,56 @@ import {URL_SERVICIOS} from '../../config/config';
 })
 export class SendToUrgenciesComponent implements OnInit {
   @Input() id: any;
-  motivo: String = '';
-  prioridad: String = '';
+  urgencyForm: FormGroup;
 
-  private modalRef: NgbModalRef;
+  public modalRef: NgbModalRef;
 
   constructor(private modalService: NgbModal, public http: HttpClient) {
   }
 
   open(content) {
+    this.createForm();
     this.modalRef = this.modalService.open(content);
   }
 
   urgencies() {
-    let idMedCenter = localStorage.getItem('idMedicalCenter');
+    const idMedCenter = localStorage.getItem('idMedicalCenter');
 
-    let data = {
-      'Motivo': this.motivo,
-      'Prioridad': this.prioridad,
+    if (this.urgencyForm.invalid) {
+      return;
+    }
+
+    const data = {
+      'Motivo': this.urgencyForm.get('motivo').value,
+      'Prioridad': this.urgencyForm.get('prioridad').value,
       'idPaciente': this.id,
       'idCentro_medico': idMedCenter
     };
 
-    //console.log(this.motivo, this.prioridad);
-
     this.http.post(`${URL_SERVICIOS}/urgencias`, data)
-      .subscribe(data => {
-        console.log(data);
-        this.modalRef.close();
-      });
+      .subscribe(
+        (res: any) => {
+          this.modalRef.close();
+          swal('Éxito', 'El paciente ha sido registrado en urgencias', 'success');
+        },
+        ({error}) => {
+          const {code} = error;
+          if (code === 409) {
+            swal('Algo malo ha ocurrido', error.error, 'error');
+          } else {
+            swal('Algo malo ha ocurrido', 'Error al agregar paciente', 'error');
+          }
+        }
+      );
   }
 
   ngOnInit() {
   }
 
+  createForm() {
+    this.urgencyForm = new FormGroup({
+      'prioridad': new FormControl(null, Validators.required),
+      'motivo': new FormControl('', Validators.required)
+    });
+  }
 }
